67530c761343bf764092249e2d9c8f27892e389d
Xen <96080301+mohxarei@users.noreply.github.com>
2024-12-17 01:28:40 +0330

    Update ADC0831.vhd

--- efes - digital voltmeter with ADC/FPGA/ADC0831.vhd
+++ efes - digital voltmeter with ADC/FPGA/ADC0831.vhd
@@ -3,71 +3,70 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity ADC0831 is
     port(
-        ref_clk:    in      std_logic;  --125MHZ (8ns) | 0 or 1 (for each device connected to FPGA)               
-        spi_clk:    out     std_logic;  --250KHZ (4us)  | to ADC      means each 500 ref_clk is one spi_clk         
-        spi_cs:     out     std_logic;  --chip select active low                 
-        spi_miso:   in      std_logic;  --DO to ADC                   
-        spi_data:   out     std_logic_vector(7 downto 0); --output port adc data (for saving sampled data readed from adc to it)
-                                                          --8bit sisnce micro is 8bit register
-        ADCRead:    in      std_logic;          --trigger (1 means sapmling|0 means idel) Sampling key       
-        busy:       out     std_logic           --status (1 busy) for sapling or not
+        ref_clk:  in   std_logic;  --125MHZ (8ns) | (FPGA clock)               
+        spi_clk:  out  std_logic;  --250KHZ (4us)  | to ADC 
+								   --means each 500 ref_clk is one spi_clk         
+        spi_cs:   out  std_logic;  --chip select active low                 
+        spi_miso: in   std_logic;  --Pin Do in ADC                   
+        spi_data: out  std_logic_vector(7 downto 0);
+								   --8 bit space for saving sampled data readed from ADC to it
+        ADCRead:  in   std_logic;  --trigger / Sampling key (1 means sapmling|0 means idel)      
+        busy:     out  std_logic   --status (1 busy) for sampling or not
     );
 end ADC0831;
 
 architecture Behavioral of ADC0831 is
-    type state is (IDLE, Tsetup, Read1, Read2, Stop); --making pulse p9 ADC datasheet
-                   --read1 no data, first clk syncronize (t setup)
-    signal FSM: state := IDLE; --data type: state (FSM changable)
-    signal spi_clk_counter: integer range 0 to 500 := 0; --data type: integer   
-    signal temporaryData: std_logic_vector(8 downto 0) := (others => '0'); --data type: std_logic_vector
-                                                            --first data (9's) is when setting timing so it's not valid
-    signal dataIndex: integer range 0 to 8 := 8;
-    signal spi_clk_int: std_logic := '0'; --data type: std_logic
+    type state is (IDLE, Tsetup, Read1, Read2, Stop);    	 --making suitable pulse to read from ADC
+    signal FSM: state := IDLE;     						 	 --data type: state (FSM changable)
+    signal spi_clk_counter: integer range 0 to 500 := 0; 	 --data type: integer   
+    signal temporaryData: std_logic_vector(8 downto 0) := (others => '0');
+    signal dataIndex: integer range 0 to 8 := 8; --9th index is for timing setting so it's not valid
+    signal spi_clk_int: std_logic := '0';
 
 begin
-    spi_clk <= spi_clk_int; --spi_clk=0
-    process(ref_clk) --process sensitive to ref_clk 125MHz 
+    spi_clk <= spi_clk_int; 								 --spi_clk=0
+    process(ref_clk) 				                         --process sensitive to ref_clk (125MHz) 
     begin
-        if rising_edge(ref_clk) then --working on rising(positive) edge / making synchrone data with ref_clk
+        if rising_edge(ref_clk) then --working on rising(positive) edge / synchrone data with ref_clk
             case FSM is
-                when IDLE => 
+                when IDLE =>		 						 --not working
                     spi_cs <= '1';                    
                     spi_clk_int <= '0';
                     spi_clk_counter <= 0;
                     dataIndex <= 8;                   
-                    busy <= '0';    --one bit                  
-                    temporaryData<= (others => '0'); --vector make 0 all indexes
-                    if ADCRead = '1' then             
+                    busy <= '0';     --'one bit'                  
+                    temporaryData<= (others => '0'); 		 --make 0 all vector's indexes
+                    if ADCRead = '1' then     		         --when sampling key is pressed initialize        
                         FSM <= Tsetup;
                         spi_cs <= '0';                
                         busy <= '1';                  
                     end if;
 
-                when Tsetup =>    --next time process running comes to Tsetup
-                    spi_clk_counter <= spi_clk_counter + 1; --spi_clk_counter ++
-                    if spi_clk_counter = 31 then   --(1/125Mhz) *31 = 248ns wait for Tsetup
-                        FSM <= Read1; --next time FSM goes to Read1
-                        spi_clk_int <= '1'; --spi_clk = 1
-                        spi_clk_counter <= 0; --initialize
+                when Tsetup =>                               --next time process running comes to Tsetup
+                    spi_clk_counter <= spi_clk_counter + 1;	 --spi_clk_counter ++
+                    if spi_clk_counter = 31 then   			 --(1/125Mhz) *31 = 248ns wait for Tsetup
+                        FSM <= Read1; 						 --next time FSM goes to Read1
+                        spi_clk_int <= '1'; 				 --spi_clk = 1
+                        spi_clk_counter <= 0; 				 --initialize
                     end if;
 
-                when Read1 =>  --in fisrt half clock data is not valid!
-                    spi_clk_int <= '1'; --making positive / spi_clk spi_clk = 1
-                    spi_clk_counter <= spi_clk_counter + 1; --spi_clk_counter ++
-                    if spi_clk_counter = 250 then --(250 * 8ns = 2us) making first half positive pulse spi_clk =1
-                        spi_clk_int <= '0'; --spi_clk = 0
-                        spi_clk_counter <= 0; --initialize
-                        FSM <= Read2; --next time FSM goes to Read2
-                    end if;
-
-                when Read2 => --making spi_clk
+                when Read1 =>  								 --in first positive clock no data is valid!
+                    spi_clk_int <= '1'; 					 --spi_clk = 1
                     spi_clk_counter <= spi_clk_counter + 1;  --spi_clk_counter ++
-                    if spi_clk_counter = 250 then --(250 * 8ns = 2us) making half spi_clk pulse
-                        spi_clk_int <= not spi_clk_int; -- making clock +-
+                    if spi_clk_counter = 250 then  			 --(250*8ns=2us) making first positive spi_clk=1
+                        spi_clk_int <= '0'; 				 --spi_clk = 0
+                        spi_clk_counter <= 0;
+                        FSM <= Read2; 						 --next time FSM goes to Read2
+                    end if;
+
+                when Read2 => 								
+                    spi_clk_counter <= spi_clk_counter + 1; 
+                    if spi_clk_counter = 250 then 			 --(250 * 8ns = 2us) time for half spi_clk pulse
+                        spi_clk_int <= not spi_clk_int; 	 -- making spi_clk +-
                         spi_clk_counter <= 0;
                     end if;
-                    if spi_clk_int = '0' and spi_clk_counter =100 then  -- = tpd  (after 8ns*100=800ns data is avalible)   
-                        temporaryData(dataIndex) <= spi_miso; --0 or 1 from MISO goes in index 8 temporaryData
+                    if spi_clk_int = '0' and spi_clk_counter =100 then--Tpd(after 8ns*100=800ns data is Valid)   
+                        temporaryData(dataIndex) <= spi_miso;--MISO(data) goes in 8th index of temporaryData
                         if dataIndex = 0 then     
                             FSM <= Stop;
                             spi_clk_counter <= 0;
@@ -75,15 +74,15 @@ begin
                             dataIndex <= dataIndex - 1;
                         end if;
                     end if;                    
-                when Stop => --one clk more for CS and data stop
-                    spi_clk_counter <= spi_clk_counter + 1;   --spi_clk_counter ++
-                    if spi_clk_counter = 250 then --waint for one spi_clk more
+                when Stop => 								 --one clk more for CS and data for stop
+                    spi_clk_counter <= spi_clk_counter + 1;  --spi_clk_counter ++
+                    if spi_clk_counter = 250 then 			 --waint for one spi_clk more
                         spi_clk_int <= not spi_clk_int;
                         spi_clk_counter <= 0;
                         if spi_clk_int = '0' then
                             spi_cs <= '1';                       
-                            spi_data <= temporaryData(7 downto 0);    --first data (9's) is when setting timing so it's not valid                      
-                            FSM <= IDLE;    --next time FSM goes to IDEL                       
+                            spi_data <= temporaryData(7 downto 0);	--9th data is for time setting/invalid!                      
+                            FSM <= IDLE;    						--next time FSM goes to IDEL                       
                         end if;
                     end if;
             end case;
