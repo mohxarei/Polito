57bb8dd2fbac71313ecdc40ac3e5c74627fd234a
Xen <96080301+mohxarei@users.noreply.github.com>
2025-01-03 15:13:32 +0330

    Delete Computer Architectures/20230207A/20230207A Clean Solution 5,6/20230207A XEN solution + CMT directory

--- Computer Architectures/20230207A/20230207A Clean Solution 5,6/20230207A XEN solution + CMT/P5/Objects/sample.d
+++
@@ -1,5 +0,0 @@
-./objects/sample.o: sample.c \
-  C:\Users\XEN\AppData\Local\Arm\Packs\Keil\LPC1700_DFP\2.7.1\Device\Include\LPC17xx.h \
-  C:\Users\XEN\AppData\Local\Arm\Packs\ARM\CMSIS\6.1.0\CMSIS\Core\Include\core_cm3.h \
-  C:\Users\XEN\AppData\Local\Arm\Packs\Keil\LPC1700_DFP\2.7.1\Device\Include\system_LPC17xx.h \
-  led\led.h button\button.h timer\timer.h
