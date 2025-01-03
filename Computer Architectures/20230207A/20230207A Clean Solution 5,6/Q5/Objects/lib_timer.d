f253e1c74b5261069bba6c3594a195b5aac01ad9
Xen <96080301+mohxarei@users.noreply.github.com>
2025-01-03 15:14:02 +0330

    Delete Computer Architectures/20230207A/20230207A Clean Solution 5,6 directory

--- Computer Architectures/20230207A/20230207A Clean Solution 5,6/Q5/Objects/lib_timer.d
+++
@@ -1,5 +0,0 @@
-./objects/lib_timer.o: timer\lib_timer.c \
-  C:\Users\XEN\AppData\Local\Arm\Packs\Keil\LPC1700_DFP\2.7.1\Device\Include\lpc17xx.h \
-  C:\Users\XEN\AppData\Local\Arm\Packs\ARM\CMSIS\6.1.0\CMSIS\Core\Include\core_cm3.h \
-  C:\Users\XEN\AppData\Local\Arm\Packs\Keil\LPC1700_DFP\2.7.1\Device\Include\system_LPC17xx.h \
-  timer\timer.h
