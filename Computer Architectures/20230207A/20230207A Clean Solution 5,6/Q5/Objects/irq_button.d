fc9ab43792f94b152bee87b577c2b65a7b53486f
Xen <96080301+mohxarei@users.noreply.github.com>
2025-01-03 15:08:44 +0330

    Add files via upload

---
+++ Computer Architectures/20230207A/20230207A Clean Solution 5,6/Q5/Objects/irq_button.d
@@ -0,0 +1,5 @@
+./objects/irq_button.o: button\IRQ_button.c button\button.h \
+  C:\Users\XEN\AppData\Local\Arm\Packs\Keil\LPC1700_DFP\2.7.1\Device\Include\lpc17xx.h \
+  C:\Users\XEN\AppData\Local\Arm\Packs\ARM\CMSIS\6.1.0\CMSIS\Core\Include\core_cm3.h \
+  C:\Users\XEN\AppData\Local\Arm\Packs\Keil\LPC1700_DFP\2.7.1\Device\Include\system_LPC17xx.h \
+  button\..\led\led.h
